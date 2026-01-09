cc63ef1a10f6a773c1095da76a90e7f9f76bcdf4
Simon Vaj <simonn1234321@gmail.com>
2026-01-09 06:17:24 +0100

    Update mimir files

--- mimir.c
+++ mimir.c
@@ -1,5 +1,11 @@
 #include "mimir.h"
 
+#if !defined(_WIN32)
+#include <sys/mman.h>
+#include <unistd.h>
+#include <string.h>
+#endif
+
 U64 mim_kilo_bytes(U64 count)
 {
 	U64 result = 1024 * count;
@@ -24,22 +30,74 @@ U64 mim_tera_bytes(U64 count)
 	return result;
 }
 
+#if defined(_WIN32)
 void* mim_alloc(U64 size)
 {
 	void* result = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 	return result;
 }
 
-B32 mim_free(void* data)
+B32 mim_free(void* data, U64 size)
 {
+	size;
 	B32 result = (B32)VirtualFree(data, 0, MEM_RELEASE);
 	return result;
+}
+#else
+void* mim_alloc(U64 size)
+{
+	void* result = mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	return result;
+}
+
+B32 mim_free(void* data, U64 size)
+{
+	B32 result = false;
+	if (data)
+	{
+		munmap(data, size);
+		result = true;
+	}
+	return result;
+}
+#endif
+
+void mim_copy(void* dst, void* src, U64 size)
+{
+	Assert(dst);
+	Assert(src);
+	memcpy(dst, src, size);
+}
+
+void mim_fill(void* data, U64 size, U64 value)
+{
+	U64 count = size / 8;
+#if defined(_WIN32)
+	__stosq((U64*)data, value, count);
+#else
+	U64* arr = (U64*)data;
+	for (U64 i = 0; i < count; i++)
+	{
+		arr[i] = value;
+	}
+#endif
 }
 
 void mim_zero(void* data, U64 size)
 {
-	Assert(size > 8);
-	__stosq(data, 0, size / 8);
+	mim_fill(data, size, 0);
+	U64 count = size / 8;
+	U8* bytes = (U8*)data;
+	for (U64 i = count * 8; i < size; i++)
+	{
+		bytes[i] = 0;
+	}
+}
+
+U64 mim_align16(U64 value)
+{
+	U64 result = (value + 15) & ~15;
+	return result;
 }
 
 U8* mim_arena_pointer(Mim_Arena* arena)
@@ -69,7 +127,7 @@ B32 mim_arena_destroy(Mim_Arena* arena)
 	Assert(arena);
 	Assert(arena->base);
 	Assert(!(arena->reserved));
-	B32 result = mim_free(arena->base);
+	B32 result = mim_free(arena->base, arena->size);
 	arena->size = 0;
 	arena->pos = 0;
 	return result;
@@ -83,36 +141,6 @@ U8* mim_arena_push_size(Mim_Arena* arena, U64 size)
 	U8* result = arena->base + arena->pos;
 	arena->pos += size;
 	return result;
-}
-
-U64 mim_arena_push_u32(Mim_Arena* arena, U32 value)
-{
-	U64 size = sizeof(U32);
-	U32* dst = (U32*)mim_arena_push_size(arena, size);
-	*dst = value;
-	return size;
-}
-
-U64 mim_arena_push_u64(Mim_Arena* arena, U64 value)
-{
-	U64 size = sizeof(U64);
-	U64* dst = (U64*)mim_arena_push_size(arena, size);
-	*dst = value;
-	return size;
-}
-
-U64 mim_arena_push_value(Mim_Arena* arena, U64 size, U8* value)
-{
-	U8* dst = (U8*)mim_arena_push_size(arena, size);
-	mim_copy(dst, value, size);
-	return size;
-}
-
-U64 mim_arena_push_buffer(Mim_Arena* arena, U64 size, U8* data)
-{
-	mim_arena_push_u64(arena, size);
-	mim_arena_push_value(arena, size, data);
-	return size + sizeof(size);
 }
 
 void mim_arena_pop_size(Mim_Arena* arena, U64 size)
@@ -174,28 +202,15 @@ void mim_scratch_end(Mim_ScratchArena* scratch)
 	arena->scratch_count--;
 }
 
-void mim_copy(void* dst, void* src, U64 size)
+void mim_begin_serializing(Mim_WriteBuffer* write_buffer, Mim_Arena* arena)
 {
-	Assert(dst);
-	Assert(src);
-	memcpy(dst, src, size);
-}
-
-U64 mim_align16(U64 value)
-{
-	U64 result = (value + 15) & ~15;
-	return result;
-}
-
-Mim_WriteBuffer mim_begin_serializing(Mim_Arena* arena)
-{
+	Assert(write_buffer);
 	Assert(arena);
-	Mim_WriteBuffer write_buffer;
-	write_buffer.size = arena->size;
-	write_buffer.pos = 0;
-	write_buffer.data = mim_arena_reserve(arena);
-	write_buffer.arena = arena;
-	return write_buffer;
+	Assert(arena->size >= arena->pos);
+	write_buffer->size = arena->size - arena->pos;
+	write_buffer->pos = 0;
+	write_buffer->base = mim_arena_reserve(arena);
+	write_buffer->arena = arena;
 }
 
 void mim_end_serializing(Mim_WriteBuffer* write_buffer)
@@ -205,47 +220,67 @@ void mim_end_serializing(Mim_WriteBuffer* write_buffer)
 	Assert(write_buffer->arena->reserved);
 	Assert(write_buffer->pos <= write_buffer->size);
 	mim_arena_commit(write_buffer->arena, write_buffer->pos);
+	write_buffer->size = write_buffer->pos;
 	write_buffer->pos = 0;
 }
 
-void mim_write_data(Mim_WriteBuffer* buffer, U64 size, U8* data)
+U8* mim_write_buffer_pointer(Mim_WriteBuffer* write_buffer)
 {
-	Assert(buffer);
-	Assert(buffer->data);
+	Assert(write_buffer);
+	Assert(write_buffer->base);
+	Assert(write_buffer->pos <= write_buffer->size);
+	U8* result = write_buffer->base + write_buffer->pos;
+	return result;
+}
+
+// Internal
+void mim_write_buffer_push_size(Mim_WriteBuffer* write_buffer, U64 size)
+{
+	Assert(write_buffer->pos + size <= write_buffer->size);
+	write_buffer->pos += size;
+}
+
+void mim_write_data(Mim_WriteBuffer* write_buffer, U8* data, U64 size)
+{
 	Assert(data);
-	Assert(buffer->pos <= buffer->size);
-	Assert(buffer->pos + size <= buffer->size);
-	U8* dst = buffer->data + buffer->pos;
+	Assert(write_buffer->arena->reserved);
+	U8* dst = mim_write_buffer_pointer(write_buffer);
 	mim_copy(dst, data, size);
-	buffer->pos += size;
+	mim_write_buffer_push_size(write_buffer, size);
 }
 
-void mim_write_u32(Mim_WriteBuffer* buffer, U32 u32)
+void mim_write_u32(Mim_WriteBuffer* write_buffer, U32 value)
 {
-	Assert(buffer);
-	Assert(buffer->data);
-	Assert(buffer->pos <= buffer->size);
-	Assert(buffer->pos + sizeof(U32) <= buffer->size);
-	U32* dst = (U32*)(buffer->data + buffer->pos);
-	*dst = u32;
-	buffer->pos += sizeof(U32);
+	U32* dst = (U32*)mim_write_buffer_pointer(write_buffer);
+	*dst = value;
+	mim_write_buffer_push_size(write_buffer, sizeof(U32));
 }
 
-void mim_write_u64(Mim_WriteBuffer* buffer, U64 u64)
+void mim_write_u64(Mim_WriteBuffer* write_buffer, U64 value)
 {
-	Assert(buffer);
-	Assert(buffer->data);
-	Assert(buffer->pos <= buffer->size);
-	Assert(buffer->pos + sizeof(U64) <= buffer->size);
-	U64* dst = (U64*)(buffer->data + buffer->pos);
-	*dst = u64;
-	buffer->pos += sizeof(U64);
+	U64* dst = (U64*)mim_write_buffer_pointer(write_buffer);
+	*dst = value;
+	mim_write_buffer_push_size(write_buffer, sizeof(U64));
 }
 
-void mim_write_buffer(Mim_WriteBuffer* buffer, U64 size, U8* data)
+void mim_write_f32(Mim_WriteBuffer* write_buffer, F32 value)
 {
-	mim_write_u64(buffer, size);
-	mim_write_data(buffer, size, data);
+	F32* dst = (F32*)mim_write_buffer_pointer(write_buffer);
+	*dst = value;
+	mim_write_buffer_push_size(write_buffer, sizeof(F32));
+}
+
+void mim_write_f64(Mim_WriteBuffer* write_buffer, F64 value)
+{
+	F64* dst = (F64*)mim_write_buffer_pointer(write_buffer);
+	*dst = value;
+	mim_write_buffer_push_size(write_buffer, sizeof(F64));
+}
+
+void mim_write_buffer(Mim_WriteBuffer* write_buffer, U8* data, U64 size)
+{
+	mim_write_u64(write_buffer, size);
+	mim_write_data(write_buffer, data, size);
 }
 
 Mim_ReadBuffer mim_create_read_buffer(Mim_Buffer* buffer)
@@ -253,66 +288,110 @@ Mim_ReadBuffer mim_create_read_buffer(Mim_Buffer* buffer)
 	Mim_ReadBuffer result;
 	result.pos = 0;
 	result.size = buffer->size;
-	result.data = buffer->data;
+	result.base = buffer->data;
 	return result;
 }
 
-B32 mim_read_data(U8* out, Mim_ReadBuffer* buffer, U64 size)
+U8* mim_read_buffer_pointer(Mim_ReadBuffer* read_buffer)
+{
+	Assert(read_buffer);
+	Assert(read_buffer->base);
+	Assert(read_buffer->pos <= read_buffer->size);
+	U8* result = read_buffer->base + read_buffer->pos;
+	return result;
+}
+
+B32 mim_read_buffer_can_push_size(Mim_ReadBuffer* read_buffer, U64 size)
+{
+	B32 valid_pos = read_buffer->pos <= read_buffer->size;
+	B32 result = valid_pos && (read_buffer->pos + size) <= read_buffer->size;
+	return result;
+}
+
+B32 mim_read_data(U8* out, Mim_ReadBuffer* read_buffer, U64 size)
 {
 	Assert(out);
-	Assert(buffer);
+	Assert(read_buffer);
 	B32 result = false;
-	if (buffer->pos <= buffer->size && (buffer->pos + size) <= buffer->size)
+	if (mim_read_buffer_can_push_size(read_buffer, size))
 	{
-		U8* data = buffer->data + buffer->pos;
+		U8* data = read_buffer->base + read_buffer->pos;
 		mim_copy(out, data, size);
-		buffer->pos += size;
+		read_buffer->pos += size;
 		result = true;
 	}
 	return result;
 }
 
-B32 mim_read_u32(U32* out, Mim_ReadBuffer* buffer)
+B32 mim_read_u32(U32* out, Mim_ReadBuffer* read_buffer)
 {
 	Assert(out);
-	Assert(buffer);
+	Assert(read_buffer);
 	B32 result = false;
 	U64 size = sizeof(U32);
-	if (buffer->pos <= buffer->size && (buffer->pos + size) <= buffer->size)
+	if (mim_read_buffer_can_push_size(read_buffer, size))
 	{
-		U32* data = (U32*)(buffer->data + buffer->pos);
+		U32* data = (U32*)(read_buffer->base + read_buffer->pos);
 		*out = *data;
-		buffer->pos += size;
+		read_buffer->pos += size;
 		result = true;
 	}
 	return result;
 }
 
-B32 mim_read_u64(U64* out, Mim_ReadBuffer* buffer)
+B32 mim_read_u64(U64* out, Mim_ReadBuffer* read_buffer)
 {
 	Assert(out);
-	Assert(buffer);
+	Assert(read_buffer);
 	B32 result = false;
 	U64 size = sizeof(U64);
-	if (buffer->pos <= buffer->size && (buffer->pos + size) <= buffer->size)
+	if (mim_read_buffer_can_push_size(read_buffer, size))
 	{
-		U64* data = (U64*)(buffer->data + buffer->pos);
+		U64* data = (U64*)(read_buffer->base + read_buffer->pos);
 		*out = *data;
-		buffer->pos += size;
+		read_buffer->pos += size;
 		result = true;
 	}
 	return result;
 }
 
-B32 mim_read_buffer(U64* size, U8* data, Mim_ReadBuffer* buffer)
+B32 mim_read_f32(F32* out, Mim_ReadBuffer* read_buffer)
 {
-	Assert(size);
-	Assert(data);
-	Assert(buffer);
+	Assert(out);
+	Assert(read_buffer);
 	B32 result = false;
-	if (mim_read_u64(size, buffer) && mim_read_data(data, buffer, *size))
+	U64 size = sizeof(F32);
+	if (mim_read_buffer_can_push_size(read_buffer, size))
 	{
+		F32* data = (F32*)(read_buffer->base + read_buffer->pos);
+		*out = *data;
+		read_buffer->pos += size;
 		result = true;
 	}
+	return result;
+}
+
+B32 mim_read_f64(F64* out, Mim_ReadBuffer* read_buffer)
+{
+	Assert(out);
+	Assert(read_buffer);
+	B32 result = false;
+	U64 size = sizeof(F64);
+	if (mim_read_buffer_can_push_size(read_buffer, size))
+	{
+		F64* data = (F64*)(read_buffer->base + read_buffer->pos);
+		*out = *data;
+		read_buffer->pos += size;
+		result = true;
+	}
+	return result;
+}
+
+B32 mim_read_buffer(U8* output_data, U64* output_size, Mim_ReadBuffer* buffer)
+{
+	Assert(output_data);
+	Assert(output_size);
+	Assert(buffer);
+	B32 result = mim_read_u64(output_size, buffer) && mim_read_data(output_data, buffer, *output_size);
 	return result;
 }
--- mimir.h
+++ mimir.h
@@ -3,27 +3,56 @@
 #if defined(_WIN32)
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-typedef INT8         I8;
-typedef INT16        I16;
-typedef INT32        I32;
-typedef INT64        I64;
+
 typedef UINT8        U8;
 typedef UINT16       U16;
 typedef UINT32       U32;
 typedef UINT64       U64;
 typedef unsigned int uint;
+
+typedef INT8         I8;
+typedef INT16        I16;
+typedef INT32        I32;
+typedef INT64        I64;
+
 typedef UINT32       B32;
+
+typedef float        F32;
+typedef double       F64;
+#else
+#include <stdint.h>
+
+typedef uint8_t      U8;
+typedef uint16_t     U16;
+typedef uint32_t     U32;
+typedef uint64_t     U64;
+typedef unsigned int uint;
+
+typedef int8_t       I8;
+typedef int16_t      I16;
+typedef int32_t      I32;
+typedef int64_t      I64;
+
+typedef uint32_t     B32;
+
 typedef float        F32;
 typedef double       F64;
 #endif
 
+#define true (1)
+#define false (0)
+
 #if !defined(ENCLOSE)
-#define ENCLOSE_IMPL(begin, end, index) for (U32 index = ((begin), 0); !index; ((end), index++))
-#define ENCLOSE(begin, end) ENCLOSE_IMPL(begin, end, base_i ## __COUNTER__)
+#define MIM_CONCAT(a, b) a ## b
+#define ENCLOSE_IMPL2(begin, end, index) for (U32 index = ((begin), 0); !index; ((end), index++))
+#define ENCLOSE_IMPL(begin, end, index, counter) ENCLOSE_IMPL2(begin, end, MIM_CONCAT(index, counter))
+#define ENCLOSE(begin, end) ENCLOSE_IMPL(begin, end, base_index_, __COUNTER__)
 #endif
 #if !defined(DEFER)
-#define DEFER_IMPL(end, index) for (U32 index = 0; !index; ((end), index++))
-#define DEFER(end) DEFER_IMPL(end, base_i ## __COUNTER__)
+#define MIM_CONCAT(a, b) a ## b
+#define DEFER_IMPL2(end, index) for (U32 index = 0; !index; ((end), index++))
+#define DEFER_IMPL(end, index, counter) DEFER_IMPL2(end, MIM_CONCAT(index, counter))
+#define DEFER(end) DEFER_IMPL(end, base_index_, __COUNTER__)
 #endif
 #if !defined(Assert)
 #define Assert(expr)
@@ -56,7 +85,6 @@ Mim_ScratchArena;
 
 #define MIM_PUSH_ARRAY(arena, type, count) (type*)mim_arena_push_size((arena), sizeof(type) * (count))
 #define MIM_PUSH(arena, type) (type*)mim_arena_push_size((arena), sizeof(type))
-#define MIM_PUSH_VALUE(arena, type, value) mim_arena_push_value((arena), sizeof(type), value)
 
 #define MIM_MAX_U8  0xff
 #define MIM_MAX_U16 0xffff
@@ -69,7 +97,11 @@ U64 mim_giga_bytes(U64 count);
 U64 mim_tera_bytes(U64 count);
 
 void* mim_alloc(U64 size);
-B32 mim_free(void* data);
+B32 mim_free(void* data, U64 size);
+void mim_copy(void* dst, void* src, U64 size);
+void mim_fill(void* data, U64 size, U64 value);
+void mim_zero(void* data, U64 size);
+U64 mim_align16(U64 size);
 U8* mim_arena_pointer(Mim_Arena* arena);
 Mim_Arena mim_arena_create(U64 size);
 B32 mim_arena_destroy(Mim_Arena* arena);
@@ -80,9 +112,6 @@ U8* mim_arena_reserve(Mim_Arena* arena);
 void mim_arena_commit(Mim_Arena* arena, U64 size);
 Mim_ScratchArena mim_scratch_begin(Mim_Arena* arena);
 void mim_scratch_end(Mim_ScratchArena* scratch);
-void mim_copy(void* dst, void* src, U64 size);
-void mim_zero(void* data, U64 size);
-U64 mim_align16(U64 size);
 
 /** Serialization *********************************************************************************/
 
@@ -90,30 +119,36 @@ typedef struct
 {
 	U64 size;
 	U64 pos;
-	U8* data;
+	U8* base;
 	Mim_Arena* arena;
 }
 Mim_WriteBuffer;
 
-#define MIM_SERIALIZE(write_buffer, arena) Mim_WriteBuffer write_buffer; ENCLOSE(write_buffer = mim_begin_serializing(arena), mim_end_serializing(&write_buffer))
+#define MIM_SERIALIZE(write_buffer, arena) ENCLOSE(mim_begin_serializing(write_buffer, arena), mim_end_serializing(write_buffer))
 
-Mim_WriteBuffer mim_begin_serializing(Mim_Arena* arena);
+void mim_begin_serializing(Mim_WriteBuffer* write_buffer, Mim_Arena* arena);
 void mim_end_serializing(Mim_WriteBuffer* write_buffer);
-void mim_write_data(Mim_WriteBuffer* buffer, U64 size, U8* data);
-void mim_write_u32(Mim_WriteBuffer* buffer, U32 u32);
-void mim_write_u64(Mim_WriteBuffer* buffer, U64 u64);
-void mim_write_buffer(Mim_WriteBuffer* buffer, U64 size, U8* data);
+U8* mim_write_buffer_pointer(Mim_WriteBuffer* write_buffer);
+void mim_write_data(Mim_WriteBuffer* write_buffer, U8* data, U64 size);
+void mim_write_u32(Mim_WriteBuffer* write_buffer, U32 value);
+void mim_write_u64(Mim_WriteBuffer* write_buffer, U64 value);
+void mim_write_f32(Mim_WriteBuffer* write_buffer, F32 value);
+void mim_write_f64(Mim_WriteBuffer* write_buffer, F64 value);
+void mim_write_buffer(Mim_WriteBuffer* write_buffer, U8* data, U64 size);
 
 typedef struct
 {
 	U64 size;
 	U64 pos;
-	U8* data;
+	U8* base;
 }
 Mim_ReadBuffer;
 
 Mim_ReadBuffer mim_create_read_buffer(Mim_Buffer* buffer);
+U8* mim_read_buffer_pointer(Mim_ReadBuffer* read_buffer);
 B32 mim_read_data(U8* out, Mim_ReadBuffer* buffer, U64 size);
 B32 mim_read_u32(U32* out, Mim_ReadBuffer* buffer);
 B32 mim_read_u64(U64* out, Mim_ReadBuffer* buffer);
-B32 mim_read_buffer(U64* size, U8* data, Mim_ReadBuffer* buffer);
+B32 mim_read_f32(F32* out, Mim_ReadBuffer* buffer);
+B32 mim_read_f64(F64* out, Mim_ReadBuffer* buffer);
+B32 mim_read_buffer(U8* output_data, U64* output_size, Mim_ReadBuffer* buffer);
